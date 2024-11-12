0d81334b720f291c632c12ca017167a711795005
Sajid Jaber <sajidjaber@yahoo.com>
2024-11-12 15:18:44 -0600

    setup

--- src/components/product/feeds/best-seller-product-feed.tsx
+++ src/components/product/feeds/best-seller-product-feed.tsx
@@ -1,25 +1,19 @@
-'use client';
-
 import ProductsCarousel from '@components/product/products-carousel';
 import { useBestSellerProductsQuery } from '@framework/product/get-all-best-seller-products';
 import { LIMITS } from '@framework/utils/limits';
 import { ROUTES } from '@utils/routes';
+import { createClient } from 'src/prismicio';
 
-export default function BestSellerProductFeed({ lang }: { lang: string }) {
-  const { data, isLoading, error } = useBestSellerProductsQuery({
-    limit: LIMITS.BEST_SELLER_PRODUCTS_LIMITS,
-  });
+export default async function BestSellerProductFeed() {
+  const client = createClient();
+  const products = await client.getAllByType('products');
 
   return (
     <ProductsCarousel
-      sectionHeading="text-best-sellers"
-      categorySlug={ROUTES.PRODUCTS}
-      products={data}
-      loading={isLoading}
-      error={error?.message}
+      sectionHeading="text-best-sellers-product"
+      products={products}
       limit={LIMITS.BEST_SELLER_PRODUCTS_LIMITS}
       uniqueKey="best-sellers"
-      lang={lang}
       className={`mb-8 lg:mb-12 navTopSlider`}
     />
   );
--- src/components/product/products-carousel.tsx
+++ src/components/product/products-carousel.tsx
@@ -7,24 +7,21 @@ import useWindowSize from '@utils/use-window-size';
 import ProductCardLoader from '@components/ui/loaders/product-card-loader';
 import cn from 'classnames';
 import { getDirection } from '@utils/get-direction';
-import ProductCardV2 from "@components/product/product-cards/product-card-v2";
+import ProductCardV2 from '@components/product/product-cards/product-card-v2';
 import ProductCard from '@components/product/product-cards/product-card';
-import React from "react";
+import React from 'react';
+import { ProductsDocument } from 'prismicio-types';
 
 interface ProductsCarouselProps {
-  sectionHeading: string;
-  categorySlug?: string;
+  sectionHeading?: string;
   className?: string;
-  products?: Product[];
-  loading: boolean;
-  error?: string;
+  products?: ProductsDocument[];
   limit?: number;
   uniqueKey?: string;
-  carouselBreakpoint?: {} | any;
+  carouselBreakpoint?: any;
+  variant?: 'default' | 'sticky';
   borderCarousel?: boolean;
-  lang: string;
-    rowCarousel?: number;
-    variant?: string;
+  rowCarousel?: number;
 }
 
 const breakpoints = {
@@ -50,88 +47,25 @@ const breakpoints = {
 
 const ProductsCarousel: React.FC<ProductsCarouselProps> = ({
   sectionHeading,
-  categorySlug,
-  className = '',
   products,
-  loading,
-  error,
+  className = '',
   limit,
   uniqueKey,
   carouselBreakpoint,
-  lang,
   variant = 'default',
   borderCarousel,
-  rowCarousel= 1,
+  rowCarousel = 1,
 }) => {
-  const dir = getDirection(lang);
   return (
     <div className={cn('relative', className)}>
       {sectionHeading && (
-          <div className="mb-3 md:mb-5">
+        <div className="mb-3 md:mb-5">
           <SectionHeader
             sectionHeading={sectionHeading}
             className={cn('mb-3', {
               'block-title ': variant === 'default',
             })}
-            lang={lang}
           />
-        </div>
-      )}
-      {error ? (
-        <div className="2xl:ltr:pr-10 2xl:rtl:pl-10">
-          <Alert message={error} />
-        </div>
-      ) : (
-        <div
-          className={`heightFull relative after-item-opacity ${
-            borderCarousel != undefined
-              ? 'p-3 border border-black/10 rounded bg-white'
-              : ''
-          } `}
-        >
-          <Carousel
-            grid={{ rows: rowCarousel, fill: 'row' }}
-            breakpoints={carouselBreakpoint || breakpoints}
-            className=""
-            prevButtonClassName="start-3  -top-12 3xl:top-auto 3xl:-translate-y-2 4xl:-translate-y-10"
-            nextButtonClassName={`end-3 -top-12 3xl:top-auto transform 2xl:translate-x-0 3xl:-translate-y-2 ${
-              dir === 'rtl' ? 'xl:-translate-x-2.5' : 'xl:translate-x-2.5'
-            }`}
-            lang={lang}
-          >
-            {loading && !products?.length ? (
-              Array.from({ length: limit! }).map((_, idx) => (
-                <SwiperSlide
-                  key={`${uniqueKey}-${idx}`}
-                  className="px-1.5 md:px-2 xl:px-2.5 py-1 w-60"
-                >
-                  <ProductCardLoader uniqueKey={`${uniqueKey}-${idx}`} />
-                </SwiperSlide>
-              ))
-            ) : (
-              <>
-                {products?.map((product: any, idx) => (
-                  <SwiperSlide
-                    key={`${uniqueKey}-${idx}`}
-                  >
-                    {variant === 'cardv2' ? (
-                      <ProductCardV2
-                        key={`${uniqueKey}-${product.id}`}
-                        product={product}
-                        lang={lang}
-                      />
-                    ) : (
-                      <ProductCard
-                        key={`${uniqueKey}-${product.id}`}
-                        product={product}
-                        lang={lang}
-                      />
-                    )}
-                  </SwiperSlide>
-                ))}
-              </>
-            )}
-          </Carousel>
         </div>
       )}
     </div>
