d75dade8c08f875ccb173e45cacc8683ee4e88ee
Sajid Jaber <sajidjaber@yahoo.com>
2024-11-19 14:13:16 -0600

    fixed the mobile nav

--- src/layouts/header/mobile-menu.tsx
+++ src/layouts/header/mobile-menu.tsx
@@ -15,6 +15,7 @@ import {
   IoClose,
 } from 'react-icons/io5';
 import { useTranslation } from 'src/app/i18n/client';
+import { CategoriesDocument, SettingsDocument } from 'prismicio-types';
 
 const social = [
   {
@@ -47,15 +48,24 @@ const social = [
   },
 ];
 
-export default function MobileMenu({ lang }: { lang: string }) {
+export default function MobileMenu({
+  lang,
+  settings,
+  categories,
+}: {
+  lang: string;
+  settings: SettingsDocument;
+  categories: CategoriesDocument[];
+}) {
   const [activeMenus, setActiveMenus] = useState<any>([]);
   const { site_header } = siteSettings;
   const { closeSidebar } = useUI();
   const { t } = useTranslation(lang, 'menu');
+
   const handleArrowClick = (menuName: string) => {
     let newActiveMenus = [...activeMenus];
     if (newActiveMenus.includes(menuName)) {
-      var index = newActiveMenus.indexOf(menuName);
+      const index = newActiveMenus.indexOf(menuName);
       if (index > -1) {
         newActiveMenus.splice(index, 1);
       }
@@ -65,132 +75,60 @@ export default function MobileMenu({ lang }: { lang: string }) {
     setActiveMenus(newActiveMenus);
   };
 
-  const ListMenu = ({
-    dept,
-    data,
-    hasSubMenu,
-    menuName,
-    menuIndex,
-    className = '',
-  }: any) =>
-    data.label && (
-      <li className={`transition-colors duration-200 ${className}`}>
-        <div className="relative flex items-center justify-between">
+  const ListMenu = () => (
+    <ul className="transition-colors duration-200">
+      {categories?.map((category: CategoriesDocument) => (
+        <li
+          key={category.id}
+          className="relative flex items-center justify-between"
+        >
           <Link
-            href={`/${lang}${data.path}`}
+            href={`/${lang}/categories/${category.id}`}
             className="relative w-full py-4 transition duration-300 ease-in-out menu-item ltr:pl-5 rtl:pr-5 md:ltr:pl-7 md:rtl:pr-7 ltr:pr-4 rtl:pl-4 text-brand-dark"
           >
             <span className="block w-full" onClick={closeSidebar}>
-              {t(`${data.label}`)}
+              {category.data.title}
             </span>
           </Link>
-          {hasSubMenu && (
-            <div
-              className="cursor-pointer w-full h-8 text-[17px] px-5 shrink-0 flex items-center justify-end text-brand-dark text-opacity-80 absolute ltr:right-0 rtl:left-0 top-1/2 transform -translate-y-1/2"
-              onClick={() => handleArrowClick(menuName)}
-            >
-              <IoIosArrowDown
-                className={`transition duration-200 ease-in-out transform ${
-                  activeMenus.includes(menuName) ? '-rotate-180' : 'rotate-0'
-                }`}
-              />
-            </div>
-          )}
-        </div>
-        {hasSubMenu && (
-          <SubMenu
-            dept={dept}
-            data={data.subMenu}
-            toggle={activeMenus.includes(menuName)}
-            menuIndex={menuIndex}
-          />
-        )}
-      </li>
-    );
-
-  const SubMenu = ({ dept, data, toggle, menuIndex }: any) => {
-    if (!toggle) {
-      return null;
-    }
-
-    dept = dept + 1;
-
-    return (
-      <ul className={cn('mobile-sub-menu', dept > 2 && 'ltr:-ml-4 rtl:-mr-4')}>
-        {data?.map((menu: any, index: number) => {
-          const menuName: string = `sidebar-submenu-${dept}-${menuIndex}-${index}`;
-
-          return (
-            <ListMenu
-              dept={dept}
-              data={menu}
-              hasSubMenu={menu.subMenu}
-              menuName={menuName}
-              key={menuName}
-              menuIndex={index}
-              className={cn(
-                dept > 1 && 'ltr:pl-4 rtl:pr-4',
-                dept > 2 && 'ltr:pl-8 rtl:pr-8'
-              )}
-            />
-          );
-        })}
-      </ul>
-    );
-  };
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
-    <>
-      <div className="flex flex-col justify-between w-full h-full">
-        <div className="bg-slate-600 w-full border-b border-border-base flex justify-between items-center relative ltr:pl-5 rtl:pr-5 md:ltr:pl-7 md:rtl:pr-7 shrink-0 py-0.5">
-          <div role="button" onClick={closeSidebar} className="inline-flex">
-            <Logo />
-          </div>
-
-          <button
-            className="flex items-center justify-center px-4 py-5 text-2xl transition-opacity md:px-5 lg:py-8 focus:outline-none hover:opacity-60"
-            onClick={closeSidebar}
-            aria-label="close"
-          >
-            <IoClose className="text-brand-light" />
-          </button>
+    <div className="flex flex-col justify-between w-full h-full">
+      <div className="bg-slate-600 w-full border-b border-border-base flex justify-between items-center relative ltr:pl-5 rtl:pr-5 md:ltr:pl-7 md:rtl:pr-7 shrink-0 py-0.5">
+        <div role="button" onClick={closeSidebar} className="inline-flex">
+          <Logo />
         </div>
 
-        <Scrollbar className="flex-grow mb-auto menu-scrollbar">
-          <div className="flex flex-col px-0  text-brand-dark h-[calc(100vh_-_150px)]">
-            <ul className="mobile-menu">
-              {site_header.menu.map((menu, index) => {
-                const dept: number = 1;
-                const menuName: string = `sidebar-menu-${dept}-${index}`;
-
-                return (
-                  <ListMenu
-                    dept={dept}
-                    data={menu}
-                    hasSubMenu={menu.subMenu}
-                    menuName={menuName}
-                    key={menuName}
-                    menuIndex={index}
-                  />
-                );
-              })}
-            </ul>
-          </div>
-        </Scrollbar>
-
-        <div className="flex items-center justify-center py-5 -mx-3 border-t text-brand-light border-border-base px-7 shrink-0">
-          {social?.map((item, index) => (
-            <Link
-              href={item.link}
-              className={`text-heading mx-3 transition duration-300 ease-in text-brand-dark text-opacity-60 hover:text-brand ${item.className}`}
-              key={index}
-            >
-              <span className="sr-only">{t(`${item.title}`)}</span>
-              {item.icon}
-            </Link>
-          ))}
-        </div>
+        <button
+          className="flex items-center justify-center px-4 py-5 text-2xl transition-opacity md:px-5 lg:py-8 focus:outline-none hover:opacity-60"
+          onClick={closeSidebar}
+          aria-label="close"
+        >
+          <IoClose className="text-brand-light" />
+        </button>
       </div>
-    </>
+
+      <Scrollbar className="flex-grow mb-auto menu-scrollbar">
+        <div className="flex flex-col px-0 text-brand-dark h-[calc(100vh_-_150px)]">
+          <ListMenu />
+        </div>
+      </Scrollbar>
+
+      <div className="flex items-center justify-center py-5 -mx-3 border-t text-brand-light border-border-base px-7 shrink-0">
+        {social?.map((item, index) => (
+          <Link
+            href={item.link}
+            className={`text-heading mx-3 transition duration-300 ease-in text-brand-dark text-opacity-60 hover:text-brand ${item.className}`}
+            key={index}
+          >
+            <span className="sr-only">{t(`${item.title}`)}</span>
+            {item.icon}
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
